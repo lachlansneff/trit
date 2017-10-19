d9e5ada7ed858be598db048e1560bf18b13cdb7b
Lachlan Sneff <lachlan.sneff@gmail.com>
2017-10-19 19:21:08 -0400

    Removing all files in .gitignore

--- target/debug/libtrit.d
+++
@@ -1,1 +0,0 @@
-C:\Users\mtsne\Desktop\Projects\trit\target\debug\libtrit.rmeta: C:\Users\mtsne\Desktop\Projects\trit\src\lib.rs
--- target/debug/trit-32bb37a261dabdeb.d
+++
@@ -1,1 +0,0 @@
-C:\Users\mtsne\Desktop\Projects\trit\target\debug\trit-32bb37a261dabdeb.exe: C:\Users\mtsne\Desktop\Projects\trit\src\lib.rs
